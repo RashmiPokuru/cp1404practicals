aee4f7954c3d9dbb0841af8917d59e949155543e
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-09-29 15:52:34 +1000

    Add randoms program with answers in comments and code to produce a random number between 1 and 100 inclusive

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+"""
+CP1404/CP5632 - Prac_02
+Random Numbers
+"""
+import random
+
+print(random.randint(5, 20))  # Smallest number = 5; Largest number = 20
+print(random.randrange(3, 10, 2))  # Smallest number = 3; Largest number = 9
+print(random.uniform(2.5, 5.5))  # Smallest number = 2.5 ; Largest number = 5.5
+print(random.randint(1, 100))
