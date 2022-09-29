a24e7e44cc704c0dacf276362cb06d2930618edd
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-09-29 16:23:01 +1000

    Modify capitalist_conrad program to output print statements to the file

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -1,5 +1,5 @@
 """
-CP1404/CP5632 - Practical
+CP1404/CP5632 - Prac_03
 Capitalist Conrad wants a stock price simulator for a volatile stock.
 The price starts off at $10.00, and, at the end of every day there is
 a 50% chance it increases by 0 to 10%, and
@@ -8,7 +8,7 @@ If the price rises above $1000, or falls below $0.01, the program should end.
 The price should be displayed to the nearest cent (e.g. $33.59, not $33.5918232901)
 """
 import random
-
+OUTPUT_FILE = "stocks.txt"
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
@@ -17,8 +17,8 @@ INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
 number_of_days = 0
-print(f"Starting price: ${price:,.2f}")
-
+out_file = open(OUTPUT_FILE, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 while price >= MIN_PRICE and price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -33,4 +33,5 @@ while price >= MIN_PRICE and price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
     number_of_days += 1
     price *= (1 + price_change)
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
