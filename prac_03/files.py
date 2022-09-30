cbdad8f4cb0524a1d91fd1a29f5888bd73339b0d
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-01 09:56:26 +1000

    Add program 2 to print the name stored in the file Add docstrings to program 1 and program 2

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,8 +4,15 @@ Files
 Writes to the files and then reads them
 """
 # 1.
-
+"""Write name to name.txt."""
 name = input("Name: ")
 out_file = open("name.txt", "w")
 print(name, file=out_file)
 out_file.close()
+
+# 2.
+"""Print name in the file"""
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Your name is {name}")
