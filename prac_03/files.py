f38a37c89fdefc27f14a7f818a5d57a397292ae9
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-01 10:10:44 +1000

    Add program 3 in files program to read and then print total of first two numbers from "numbers.txt"

--- prac_03/files.py
+++ prac_03/files.py
@@ -3,16 +3,25 @@ CP1404/CP5632 - Prac_03
 Files
 Writes to the files and then reads them
 """
-# 1.
-"""Write name to name.txt."""
-name = input("Name: ")
-out_file = open("name.txt", "w")
-print(name, file=out_file)
-out_file.close()
+# # 1.
+# """Write name to name.txt."""
+# name = input("Name: ")
+# out_file = open("name.txt", "w")
+# print(name, file=out_file)
+# out_file.close()
+#
+# # 2.
+# """Print name in the file"""
+# in_file = open("name.txt", "r")
+# name = in_file.read().strip()
+# in_file.close()
+# print(f"Your name is {name}")
 
-# 2.
-"""Print name in the file"""
-in_file = open("name.txt", "r")
-name = in_file.read().strip()
-in_file.close()
-print(f"Your name is {name}")
+# 3.
+"""Read first two numbers from "numbers.txt" file and print total of them"""
+with open("numbers.txt", "r") as in_file:
+    first_number = int(in_file.readline())
+    second_number = int(in_file.readline())
+print(first_number + second_number)
+
+
