425451db471c4dd6313389aea7807ff9155d197c
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-01 10:55:39 +1000

    Modify password_checker program to count digits, special characters, lower and upper case characters in password Modify SPECIAL_CHARS_REQUIRED from FALSE to TRUE

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
+SPECIAL_CHARS_REQUIRED = True
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -36,8 +36,14 @@ def is_valid_password(password):
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-        pass
+        if char.islower():
+            count_lower += 1
+        elif char.isupper():
+            count_upper += 1
+        elif char.isdigit():
+            count_digit += 1
+        elif char in SPECIAL_CHARACTERS:
+            count_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
 
