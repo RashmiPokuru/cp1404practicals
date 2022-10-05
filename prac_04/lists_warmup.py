2a477d248c47b91631f3807c831911465fec34d2
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-06 00:17:01 +1000

    Add expressions and statements to lists_warmup program

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 - prac_04 - Lists
+Warm-Up
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+"""
+# Expressions
+numbers[0] - 3
+numbers[-1] - 2
+numbers[3] - 1
+numbers[:-1] - [3, 1, 4, 1, 5, 9]
+numbers[3:4] - [1]
+5 in numbers - True
+7 in numbers - False
+"3" in numbers - False
+numbers + [6, 5, 3] - [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+"""
+
+# Statements
+
+# 1.
+numbers[0] = 10
+# 2.
+numbers[-1] = 1
+# 3.
+print(numbers[2:])
+# 4.
+print(9 in numbers)
