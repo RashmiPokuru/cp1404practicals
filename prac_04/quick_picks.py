23989aca21ae9f112384a5984e06ea94149ea01b
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-07 00:48:28 +1000

    Modify quick_picks program to create list of strings from list of numbers Refactor row_numbers as line_numbers

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,5 +9,8 @@ MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
 number_of_picks = int(input("How many quick picks? "))
 for i in range(number_of_picks):
-    row_numbers = [randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) for j in range(NUMBER_OF_COLUMNS)]
-    print(row_numbers)
+    line_numbers = [randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) for j in range(NUMBER_OF_COLUMNS)]
+    line_strings = [f"{number:2}" for number in line_numbers]
+    print(line_numbers)
+    print(line_strings)
+
