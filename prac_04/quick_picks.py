962f515ef03a3a870b689e5e6844a943e5ee186f
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-07 01:29:55 +1000

    Modify line_numbers logic to insert non-repetitive numbers using loop

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -9,9 +9,15 @@ MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
 number_of_picks = int(input("How many quick picks? "))
 for i in range(number_of_picks):
-    line_numbers = [randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) for j in range(NUMBER_OF_COLUMNS)]
+    # line_numbers = [randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) for j in range(NUMBER_OF_COLUMNS)]
+    line_numbers = []
+    while len(line_numbers) < NUMBER_OF_COLUMNS:
+        number = randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        if number not in line_numbers:
+            line_numbers.append(number)
     line_strings = [f"{number:2}" for number in sorted(line_numbers)]
     # print(line_numbers)
     # print(line_strings)
     print(' '.join(line_strings))
+
 
