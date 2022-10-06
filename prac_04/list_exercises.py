075cc9acf19c0ef5a58f07f339a1d48954841238
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-06 16:06:54 +1000

    Add code in list_exercises program to get 5 numbers and store them in list Add docstring in the beginning of program

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Prac_04 - Lists
+Basic list operations
+print details of a list of numbers
+"""
+NUMBER_OF_NUMBERS = 5
+numbers = []
+for i in range(NUMBER_OF_NUMBERS):
+    number = int(input(f"Number: "))
+    numbers.append(number)
+# print(numbers)
+
