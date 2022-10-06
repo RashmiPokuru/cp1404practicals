aee2e8776a740a57f6c0f4e557c96160a5db6a62
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-06 16:09:59 +1000

    Modify list_exercises program to include print statements

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,4 +9,8 @@ for i in range(NUMBER_OF_NUMBERS):
     number = int(input(f"Number: "))
     numbers.append(number)
 # print(numbers)
-
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {sum(numbers)/len(numbers)}")
