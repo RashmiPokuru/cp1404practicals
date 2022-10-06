cc4870821f0b8f67915e58287561e03df66957be
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-07 00:53:02 +1000

    Modify quick_picks program to sort line_numbers list, use .join to display space in between strings

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -10,7 +10,8 @@ MAXIMUM_NUMBER = 45
 number_of_picks = int(input("How many quick picks? "))
 for i in range(number_of_picks):
     line_numbers = [randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) for j in range(NUMBER_OF_COLUMNS)]
-    line_strings = [f"{number:2}" for number in line_numbers]
-    print(line_numbers)
-    print(line_strings)
+    line_strings = [f"{number:2}" for number in sorted(line_numbers)]
+    # print(line_numbers)
+    # print(line_strings)
+    print(' '.join(line_strings))
 
