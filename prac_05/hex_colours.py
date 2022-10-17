624409037156d16cb2e82a6758ad65c4e823ca1e
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-18 03:33:55 +1000

    complete hex_colours program

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 Prac 05
+get code from input colour name
+"""
+
+COLOUR_TO_CODE = {"BlueViolet": "#8a2be2", "Burlywood1": "#ffd39b", "Burlywood": "#deb887",
+                  "Carmine Pink": "#eb4c42", "Pink1": "#ffb5c5", "Ruby": "#e0115f", "Silver Pink": "#c4aead",
+                  "Snow4": "#8b8989", "SteelBlue": "#4682b4", "Zomp": "#39a78e"}
+# print(COLOUR_TO_CODE)
+
+# COLOUR_UPPER_TO_CODE = {colour.upper():code for (colour, code) in COLOUR_TO_CODE.items()}
+# print(COLOUR_UPPER_TO_CODE)
+
+colour = input("Enter colour name: ")
+while colour != "":
+    try:
+        code = {colour.upper(): code for (colour, code) in COLOUR_TO_CODE.items()}.get(colour.upper())
+        print(f"{colour} is {code}")
+    except KeyError:
+        print("Invalid colour name")
+    colour = input("Enter colour name: ")
+
+
