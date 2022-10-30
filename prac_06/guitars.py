0e099852b8f642a0c4f1e96858f38778ec461697
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 18:25:56 +1000

    Comment test lines

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -10,21 +10,21 @@ def main():
     """Store user's guitars in list until they enter blank name and then display details"""
     guitars = []
     print("My guitars!")
-    # name = input("Name: ")
-    # while name != "":
-    #     year = int(input("Year: "))
-    #     cost = float(input("Cost: $"))
-    #     guitar = Guitar(name, year, cost)
-    #     guitars.append(guitar)
-    #     print(f"{guitar} added.")
-    #     name = input("Name:")
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name:")
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("These are my guitars:")
     maximum_name_length = max(len(guitar.name) for guitar in guitars)
     maximum_cost_length = max(len(str(f"{guitar.cost:,.2f}")) for guitar in guitars)
-    print(maximum_name_length)
-    print(maximum_cost_length)
+    # print(maximum_name_length)
+    # print(maximum_cost_length)
     guitars.sort(key=attrgetter("year"))
     for i, guitar in enumerate(guitars, 1):
         vintage_display = " (vintage)" if guitar.is_vintage() else ""
