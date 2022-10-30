3bd61735d2804e9dba62e495243d8e515ebb645c
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 16:38:06 +1000

    Add main() with code in it

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -4,10 +4,16 @@ CP1404/CP5632 Practical - Client code to use the ProgrammingLanguage class.
 
 from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
-languages = [python, ruby, visual_basic]
-print(type(languages))
-print(languages)
+
+def main():
+    """Add programming languages and print the names of all the languages with dynamic typing"""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+    languages = [python, ruby, visual_basic]
+    print(type(languages))
+    print(languages)
+
+
+main()
