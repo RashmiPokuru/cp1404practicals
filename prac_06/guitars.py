e45077299039c617474f5b84ce32ef2a5ac77137
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 18:27:39 +1000

    Add space for prompt string for name input

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -17,7 +17,7 @@ def main():
         guitar = Guitar(name, year, cost)
         guitars.append(guitar)
         print(f"{guitar} added.")
-        name = input("Name:")
+        name = input("Name: ")
     # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("These are my guitars:")
