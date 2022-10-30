989df3788368725446394d977b024e985c58618f
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 18:10:55 +1000

    Calculate maximum_name_length

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,6 +19,8 @@ def main():
     guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
     guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
     print("These are my guitars:")
+    maximum_name_length = max(len(guitar.name) for guitar in guitars)
+    print(maximum_name_length)
     for i, guitar in enumerate(guitars, 1):
         vintage_display = " (vintage)" if guitar.is_vintage() else ""
         print(f"Guitar {i}: {guitar.name}, worth $ {guitar.cost}{vintage_display}")
