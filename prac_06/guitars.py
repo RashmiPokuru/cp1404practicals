33cc2202e24e6bfc0b7a4c1971c3384de98da1a2
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 18:02:09 +1000

    Add input guitar details to list

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,3 +6,16 @@ from prac_06.guitar import Guitar
 
 def main():
     """Store user's guitars in list until they enter blank name and then display details"""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name:")
+
+
+main()
