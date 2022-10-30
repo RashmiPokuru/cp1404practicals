05a8905ca3ad90fe2e429a8d3f7e4e8968092773
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 16:53:08 +1000

    Add __init__ method with defaults

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -1,4 +1,20 @@
 """CP1404/CP5632 Practical - Guitar class.
 Estimate = 2 hours
 Current_time = 4:46 PM
-Actual duration = """
+Actual duration =
+"""
+
+
+class Guitar:
+    """Represent a guitar"""
+
+    def __init__(self, name="", year=0, cost=0):
+        """
+        Construct a Guitar from the given values
+        name: string, name of guitar
+        year: integer, year of guitar
+        cost: float, cost of guitar
+        """
+        self.name = name
+        self.year = year
+        self.cost = cost
