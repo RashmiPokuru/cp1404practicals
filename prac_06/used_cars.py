2844d8481830e842da6676451e605784c51d2e50
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 10:11:59 +1000

    Drive limo 115 km

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -13,6 +13,7 @@ def main():
     my_car.drive(30)
     limo = Car(100)
     limo.add_fuel(20)
+    limo.drive(115)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
     print(f"Limo has fuel: {limo.fuel}")
