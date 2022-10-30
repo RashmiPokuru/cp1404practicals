f7037af75da176843e91eeb8864a70363c7cd20a
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-10-30 15:57:23 +1000

    Copy code from practical

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,3 +3,8 @@ CP1404/CP5632 Practical - Client code to use the ProgrammingLanguage class.
 """
 
 from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
