8166b403a36063d31608bca3ceb175da9461a37e
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-03 15:15:32 +1000

    Add __init__ method

--- prac_07/project.py
+++ prac_07/project.py
@@ -3,3 +3,14 @@ CP1404/CP5632 Practical - Project class
 Estimate - 2 hours
 """
 
+
+class Project:
+    """Represent a project"""
+
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialise a project"""
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
