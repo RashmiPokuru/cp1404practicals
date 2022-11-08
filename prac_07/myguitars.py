a6cd7291fa8acfb4d640772b442b3fdc4071641b
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-09 05:54:02 +1000

    Add sort to sort guitars on year

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,6 @@
 CP1404/CP5632 Practical - Client code to use the Guitar class.
 """
 from prac_06.guitar import Guitar
-import csv
 
 FILE_NAME = "guitars.csv"
 
@@ -10,6 +9,7 @@ FILE_NAME = "guitars.csv"
 def main():
     """Store guitars from file into list."""
     guitars = load_guitars(FILE_NAME)
+    guitars.sort()
     for guitar in guitars:
         print(guitar)
 
@@ -17,12 +17,11 @@ def main():
 def load_guitars(file_name):
     guitars = []
     with open(file_name, 'r') as in_file:
-        guitars.append(csv.reader(in_file))
         for line in in_file:
             parts = line.strip().split(',')
             guitar = Guitar(parts[0], int(parts[1]), float(parts[2]))
             guitars.append(guitar)
     return guitars
 
-main()
 
+main()
