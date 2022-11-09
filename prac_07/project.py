e627d363caa5bce17a860ecb3b5b19416437ed06
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-09 20:38:44 +1000

    Add datetime in project

--- prac_07/project.py
+++ prac_07/project.py
@@ -2,6 +2,7 @@
 CP1404/CP5632 Practical - Project class
 Estimate - 2 hours
 """
+import datetime
 
 
 class Project:
@@ -17,8 +18,9 @@ class Project:
 
     def __str__(self):
         """Return formatted project as string"""
-        return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
-               f"estimate: ${self.cost_estimate:.2f}, completion: {self.completion_percentage}%"
+        return f"{self.name}, start: {datetime.datetime.strftime(self.start_date, '%d/%m/%Y')}," \
+               f" priority {self.priority}, estimate: ${self.cost_estimate:.2f}, " \
+               f"completion: {self.completion_percentage}%"
 
     def __repr__(self):
         """return list objects in string format"""
@@ -33,9 +35,9 @@ class Project:
 
 
 def run_tests():
-    p1 = Project(name="Build Car Park", start_date="2/9/2021", priority=2, cost_estimate=600000.0,
+    p1 = Project(name="Build Car Park", start_date=datetime.datetime.strptime("9/1/2021", '%d/%m/%Y'), priority=2, cost_estimate=600000.0,
                  completion_percentage=95)
-    p2 = Project(name="Test", start_date="14/11/2021", priority=2, cost_estimate=700.0,
+    p2 = Project(name="Test", start_date=datetime.datetime.strptime("9/2/2021", '%d/%m/%Y'), priority=3, cost_estimate=700.0,
                  completion_percentage=100)
     print(p1)
     print(p1.is_complete())
