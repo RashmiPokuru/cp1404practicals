94457d1b05f167f8290f06ee0cbc758ce46a25d4
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-14 13:33:05 +1000

    Modify handle_convert() to display value in kilometers

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -5,12 +5,23 @@ Convert miles to kilometers
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 
 class MilesConverterApp(App):
+    number_of_kilometers = StringProperty()
+
     def build(self):
         self.title = "Covert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.number_of_kilometers = '1.6'
+        return self.root
+
+    def handle_convert(self):
+        try:
+            self.number_of_kilometers = str(float(self.root.ids.input_miles.text) * 1.60934)
+        except ValueError:
+            pass
 
 
 MilesConverterApp().run()
