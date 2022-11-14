2bbf7abfff1f4de854072f1bfbea2fc3db36c446
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-14 11:06:56 +1000

    Modify handle_greet() to change output_label text

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -11,7 +11,7 @@ class BoxLayoutDemo(App):
     def handle_greet(self):
         """Handle greet press"""
         print("test")
-        self.root.ids.output_label.text = "Hello"
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 
