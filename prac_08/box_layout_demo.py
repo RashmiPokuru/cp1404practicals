2e64577f843b71be72d4629adc7669c92a3b84cc
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-14 11:02:49 +1000

    Add handle_greet method to change label text

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,11 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        """Handle greet press"""
+        print("test")
+        self.root.ids.output_label.text = "Hello"
+
+
 
 BoxLayoutDemo().run()
