f5a0ce8ac2527ca6844c3dd1f23e742dd3abdd1f
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 15:37:31 +1000

    Modify my_taxi to not pass price_per_km

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -8,7 +8,7 @@ from prac_09.taxi import Taxi
 def main():
     """Test Taxi class."""
     # 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     # 2. Drive the taxi 40 km
     my_taxi.drive(40)
     # 3. Print the taxi's details and the current fare
