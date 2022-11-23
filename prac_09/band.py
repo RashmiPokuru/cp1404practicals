0b3d4a4915d5511f617ee4175cde2b5433d5fb9e
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 17:40:55 +1000

    Correct indentation

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,9 +7,8 @@ Band class
 class Band:
     """Band class with musicians and instruments."""
 
-
-def __init__(self, name=""):
-    """Initialize Band with empty collections of musicians and instruments."""
-    self.name = name
-    self.musicians = []
-    self.instruments = []
+    def __init__(self, name=""):
+        """Initialize Band with empty collections of musicians and instruments."""
+        self.name = name
+        self.musicians = []
+        self.instruments = []
