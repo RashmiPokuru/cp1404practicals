47d8ee3e3d7cb5defa80d7a8218576d5ced306a1
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 16:02:47 +1000

    Add unreliable_car_test program

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,12 @@
+"""
+CP1404/CP5632 Practical 09
+Program to test UnreliableCar class
+"""
+
+
+def main():
+    """Test Unreliable Car class for various random numbers and distances."""
+    pass
+
+
+main()
