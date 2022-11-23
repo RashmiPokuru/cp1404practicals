9446f46354d8a7a70ea4929a57f77c96051682e8
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 22:28:27 +1000

    Add current_taxi

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,10 +8,10 @@ from silver_service_taxi import SilverServiceTaxi
 MENU_STRING = "q)uit, c)hoose taxi, d)rive"
 
 
-
 def main():
     """Taxi simulator to use taxi."""
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 1), SilverServiceTaxi("Hummer", 200, 2)]
+    current_taxi = None
     print("Let's drive!")
     print(MENU_STRING)
     choice = input(">>>").upper()
