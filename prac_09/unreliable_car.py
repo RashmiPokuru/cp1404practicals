db7b2eb6d30cbc228fc067b07ede3bd305c20f96
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 15:57:12 +1000

    Add drive method to drive car based on reliability

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,12 +3,25 @@ CP1404/CP5632 Practical 09
 UnreliableCar class - class extends Car class.
 """
 from prac_09.car import Car
+from random import randint
+
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 100
 
 
 class UnreliableCar(Car):
     """UnreliableCar class extends Car class and has additional reliability attribute."""
 
     def __init__(self, name, fuel, reliability):
-        """Initialise UnreliableCar"""
+        """Initialise UnreliableCar."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car based on reliability."""
+        random_number = randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        if random_number < self.reliability:
+            distance_driven = super().drive(distance)
+        else:
+            distance_driven = 0  # Return 0 when the car is not driven
+        return distance_driven
