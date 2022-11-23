47f5fe63e4f0bad77cc47e33424220f9fafe5cb2
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 15:32:34 +1000

    Add lines of code for tasks 1 to 5

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,11 +2,22 @@
 CP1404/CP5632 Practical 09
 Test program for Taxi class
 """
+from prac_09.taxi import Taxi
 
 
 def main():
     """Test Taxi class."""
-    pass
+    # 1. Create a new taxi object, my_taxi, with name "Prius 1", 100 units of fuel and price of $1.23
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    # 2. Drive the taxi 40 km
+    my_taxi.drive(40)
+    # 3. Print the taxi's details and the current fare
+    print(my_taxi)
+    # 4. Restart the meter (start a new fare) and then drive the car 100 km
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    # 5. Print the details and the current fare
+    print(my_taxi)
 
 
 if __name__ == '__main__':
