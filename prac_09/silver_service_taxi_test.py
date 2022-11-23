ddebb467c0b99f059e982073836d1929c03e42bd
RashmiPokuru <venkatarashmi.pokuru@my.jcu.edu.au>
2022-11-23 16:51:33 +1000

    Add test code to test SilverServiceTaxi

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -2,11 +2,15 @@
 CP1404/CP5632 Practical 09
 Program to test SilverServiceTaxi class
 """
+from prac_09.silver_service_taxi import SilverServiceTaxi
 
 
 def main():
     """Test SilverServiceTaxi class"""
-    pass
+    silver_service_taxi = SilverServiceTaxi("Silver Taxi", 150, 2)
+    silver_service_taxi.drive(18)
+    print(silver_service_taxi)
+    print(silver_service_taxi.get_fare())
 
 
 main()
